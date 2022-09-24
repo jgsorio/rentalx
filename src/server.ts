944b92066a6f8015a4594963b5618d5c0f541b77
José Guilherme Sório <joseguilhermesorio@gmail.com>
2022-09-23 23:18:26 -0300

    refactor: keep routes in one file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const server = express();
 
 server.use(express.json());
-server.use('/categories', categoriesRoutes);
-server.use('/specifications', specificationsRoutes);
+server.use(router);
 
 server.listen(3333);
